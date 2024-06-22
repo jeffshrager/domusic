0119aebe2af0edc9e7dcd65d85cb33ae160f0d62
jeffshrager <jshrager@stanford.edu>
2024-06-22 15:59:39 -0700

    Added CDS music and reader

--- test.py
+++ test.py
@@ -34,11 +34,12 @@ def sine_wave(hz, peak, n_samples=sample_rate):
 # Chords
 #play_for(sum([sine_wave(440, 4096), sine_wave(880, 4096)]), 1000)
 
-# Label the three octaves around middle C:
-note_names = ["C0", ["CS0", "DF0"], "D0", ["DS0", "EF0"], "E0", "F0", ["FS0", "GF0"], "G0", ["GS0", "AF0"], "A0", ["AS0", "BF0"], "B0",
-              "C1", ["CS1", "DF1"], "D1", ["DS1", "EF1"], "E1", "F1", ["FS1", "GF1"], "G1", ["GS1", "AF1"], "A1", ["AS1", "BF1"], "B1",
-              "C2", ["CS2", "DF2"], "D2", ["DS2", "EF2"], "E2", "F2", ["FS2", "GF2"], "G2", ["GS2", "AF2"], "A2", ["AS2", "BF2"], "B2",
-              "C3"]
+# Label the three octaves around middle C (C4). 
+
+note_names = ["C3", ["CS3", "DF3"], "D3", ["DS3", "EF3"], "E3", "F3", ["FS3", "GF3"], "G3", ["GS3", "AF3"], "A3", ["AS3", "BF3"], "B3",
+              "C4", ["CS4", "DF4"], "D4", ["DS4", "EF4"], "E4", "F4", ["FS4", "GF4"], "G4", ["GS4", "AF4"], "A4", ["AS4", "BF4"], "B4",
+              "C5", ["CS5", "DF5"], "D5", ["DS5", "EF5"], "E5", "F5", ["FS5", "GF5"], "G5", ["GS5", "AF5"], "A5", ["AS5", "BF5"], "B5",
+              "C6"]
 global notes
 note2freq = {}
 
@@ -60,18 +61,62 @@ print(note2freq)
 
 # Simple version doesn't handle playing chords.
 # ToDo: Make this interpret chords, as:
-#    play_tune(["C1",["C1","E1","G1"],"EF2",["C1","EF1","G1"],...],300)
+#    play_tune(["C4",["C4","E4","G4"],"EF3",["C5","EF5","G5"],...],300)
 
 def play_tune(notes,ms=1000):
     global note2freq
     for note in notes:
         play_for(sine_wave(note2freq[note], 4096),ms)
 
-play_tune(["C2","C2","D2","C2","F2","E2","C2","C2","D2","C2","G2","F2"],300)
+#play_tune(["C4","C4","D4","C4","F4","E4","C4","C4","D4","C4","G4","F4"],300)
 
 # Play a chord by note name
-play_for(sum([sine_wave(note2freq[note], 4096) for note in ["C1","E1","G1"]]), 1000) # C Major
-play_for(sum([sine_wave(note2freq[note], 4096) for note in ["C1","EF1","G1"]]), 1000) # C Minor
+#play_for(sum([sine_wave(note2freq[note], 4096) for note in ["C4","E4","G4"]]), 1000) # C Major
+#play_for(sum([sine_wave(note2freq[note], 4096) for note in ["C4","EF4","G4"]]), 1000) # C Minor
+
+# Decoder for .csv files from: https://zenodo.org/records/4916302
+# I think that C4 = 60 (Middle C), so C3 = 48
+# According to: https://inspiredacoustics.com/en/MIDI_note_numbers_and_center_frequencies
+
+# Flatten the note_names list, using only the first entry of sublists
+flattened_note_names = []
+for note in note_names:
+    if isinstance(note, list):
+        flattened_note_names.append(note[0])
+    else:
+        flattened_note_names.append(note)
+
+# Base value for the first note
+BASE = 48
+
+# Function to map number to note name
+def get_note_name(number):
+    index = number - BASE
+    if 0 <= index < len(flattened_note_names):
+        return flattened_note_names[index]
+    else:
+        return None
+
+import csv
+
+def load_cds_csv(filepath):
+    note_strings = []
+    with open(filepath, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if len(row) > 2:
+                try:
+                    number = int(row[2])
+                    note_name = get_note_name(number)
+                    if note_name:
+                        note_strings.append(note_name)
+                except ValueError:
+                    # Handle the case where conversion to integer fails
+                    continue
+    return note_strings    
+
+# Plays Joy to the World
+play_tune(load_cds_csv("CSD/english/csv/en022a.csv"),200)
 
 # Something's wrong with the below. Need to debug at some point.  It
 # came from the original web site (as at top) but seems to be broken.
